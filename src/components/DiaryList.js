54ca9ec32a0c6eee7c8fe4c6f310b554f378611f
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-02-27 19:31:30 +0900

    home 화면 완성

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import MyButton from './MyButton';
 import { useNavigate } from 'react-router-dom';
+import MyButton from './MyButton';
+import DiaryItem from './DiaryItem';
 
 const sortOptionList = [
   { value: 'latest', name: '최신 순' },
@@ -86,11 +87,7 @@ const DiaryList = ({ diaryList }) => {
       </div>
 
       {getProcessedDiaryList().map((it) => {
-        return (
-          <div key={it.id}>
-            {it.content} {it.emotion}
-          </div>
-        );
+        return <DiaryItem key={it.id} {...it} />;
       })}
     </div>
   );
