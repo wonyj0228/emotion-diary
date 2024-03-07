e2b75b295664abc3804409017d307882d0045d6d
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-03-08 00:18:28 +0900

    edit 페이지

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,43 +1,31 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import DiaryEditor from '../components/DiaryEditor';
 
 const Edit = () => {
-  // query string은 routes path에 영향을 받지 않음
-  // http://localhost:3000/edit?id=231561&mode=dark
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const id = searchParams.get('id');
-  console.log(id);
-
-  const mode = searchParams.get('mode');
-  console.log(mode);
-
-  // 페이지를 이동시킬 수 있는 함수를 반환해줌.
-  // parameter는 path
-  // ex- 로그인 하지 않은 사람이 마이페이지를 이용하려할 때 로그인 페이지로 바꿔버릴 수 있음
   const navigate = useNavigate();
+  const diaryList = useContext(DiaryStateContext);
+  const [originData, setOriginData] = useState();
+  const { id } = useParams();
 
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      console.log(targetDiary);
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate('/', { replace: true });
+      }
+    }
+  }, [diaryList, id]);
   return (
     <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정페이지 입니다.</p>
-      <button onClick={() => setSearchParams({ who: 'yejin' })}>
-        QS 바꾸기
-      </button>
-
-      <button
-        onClick={() => {
-          navigate('/home');
-        }}
-      >
-        HOME으로 가기
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로가기
-      </button>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
