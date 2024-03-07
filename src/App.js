4d28b5e4d036651cddae6eaf8242bec943f55b2c
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-03-08 02:51:45 +0900

    localstorage 사용
    - 더미데이터 삭제
    - 초기데이터 init
    - 데이터 변경시 setItem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useEffect, useReducer, useRef } from 'react';
 
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -33,49 +33,30 @@ const reducer = (state, action) => {
       return state;
   }
 
+  localStorage.setItem('diary', JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: '오늘의 일기 1번',
-    date: 1708877732966,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: '오늘의 일기 2번',
-    date: 1708877732967,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: '오늘의 일기 3번',
-    date: 1708877732968,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: '오늘의 일기 4번',
-    date: 1708877732969,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: '오늘의 일기 5번',
-    date: 1708877732970,
-  },
-];
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
 
-  const dataId = useRef(6);
+  useEffect(() => {
+    const localData = JSON.parse(localStorage.getItem('diary'));
+
+    if (localData.length > 0) {
+      const diaryList = localData.sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({ type: 'INIT', data: diaryList });
+    }
+  }, []);
+
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
