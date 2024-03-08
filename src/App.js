4521416b53c9df9826e9065e7debc010d0384776
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-03-08 16:41:55 +0900

    버그 수정

--- src/App.js
+++ src/App.js
@@ -45,15 +45,17 @@ function App() {
   const dataId = useRef(0);
 
   useEffect(() => {
-    const localData = JSON.parse(localStorage.getItem('diary'));
+    const localData = localStorage.getItem('diary');
 
-    if (localData.length > 0) {
-      const diaryList = localData.sort(
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
         (a, b) => parseInt(b.id) - parseInt(a.id)
       );
-      dataId.current = parseInt(diaryList[0].id) + 1;
 
-      dispatch({ type: 'INIT', data: diaryList });
+      if (diaryList.length >= 1) {
+        dataId.current = parseInt(diaryList[0].id) + 1;
+        dispatch({ type: 'INIT', data: diaryList });
+      }
     }
   }, []);
 
